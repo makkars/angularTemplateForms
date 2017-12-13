df3e4fc071827b1fa5dbb7761749884006c5bdcb
eshmakk <shashank.makkar@ericsson.com>
2017-12-13 19:56:35 +0530

    complete

--- src/app/component/hero-form/hero-form.component.ts
+++ src/app/component/hero-form/hero-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Hero } from '../../model/hero';
+import { Hero }    from '../../model/hero';
 
 @Component({
   selector: 'app-hero-form',
@@ -8,15 +8,17 @@ import { Hero } from '../../model/hero';
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent {
-  submitted = false;
+
   powers = ['Really Smart', 'Super Flexible',
-    'Super Hot', 'Weather Changer'];
+            'Super Hot', 'Weather Changer'];
 
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
 
+  submitted = false;
 
   onSubmit() { this.submitted = true; }
 
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
 }
